7f05fbd6a99c3f67a9ba8b019380496d7f37cee4
MohamedALNatsh <104651138+mohamedalnatsh@users.noreply.github.com>
2026-03-04 23:48:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js'); 
 const qrcode = require('qrcode-terminal');
 const express = require('express');
+const path = require('path');   
 const app = express();
 
 const client = new Client({
@@ -12,35 +13,23 @@ const client = new Client({
     puppeteer: {
         headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
-        executablePath: '/usr/bin/chromium-browser' // المسار الصحيح للسيرفر المحلي
+        executablePath: '/usr/bin/chromium-browser' 
     }
 });
 
-let lastQr = null;
-
-client.on('qr', (qr) => {
-    lastQr = qr;
-    qrcode.generate(qr, {small: true});
-    console.log('يرجى مسح الرمز الجديد للربط');
-});
-
-client.on('ready', () => {
-    lastQr = null;
-    console.log('✅ تم الاتصال بنجاح! البوت يرد الآن.');
-});
 
 client.on('message', async msg => {
     const text = msg.body.trim().toLowerCase();
-    console.log('رسالة واصلة: ' + text);
-
+    
     if (text === 'بداية' || text === 'قائمة') {
         await msg.reply(
             "🏥 *مرحباً بك في نظام دعم مستشفى العيون*\n\n" +
             "أرسل الرقم المطلوب:\n" +
             "1. إجراءات التشغيل (SOPs)\n" +
             "2. جداول المناوبات اليومية\n" +
-            "3. تعليمات العمليات"
-        );
+            "3. تعليمات العمليات\n" +
+            "4. جدول دوام الأمن (PDF)"
+        ); 
     } 
     else if (text === '1') {
         await msg.reply("📋 *إجراءات التشغيل:* \n- تعقيم (ارسل 'تعقيم')\n- استقبال (ارسل 'استقبال')");
@@ -49,7 +38,7 @@ client.on('message', async msg => {
         await msg.reply("📅 *المناوبات:* \n- م.رشيد(الأحد 72 ساعة)\n- م.ط.د.ع.ا.م.ي شرين (أي يوم أي ساعة)\n- م.أحمد (السبت 24 ساعة)");
     }
     else if (text === '3')
-        await msg.reply()        
+        await msg.reply("*تعليمات العمليات* \n- تنظيف الشسمو \n- طرد البشر من الغرفة \n- أغلاق الأجهزة الألكترونية")        
     else if (text === 'أستقبال') {
         await msg.reply("🧴 *خطوات الأستقبال:* \n1. أبتسام عشان محترمين \n2. ما نثقل دمنا على الناس .. \n3. ما بنتأخر في الدوام لانو عنا أهل يسئلو علينا");
     }
@@ -57,8 +46,16 @@ client.on('message', async msg => {
         await msg.reply("🧴 *خطوات التعقيم:* \n1. تطهير الأسطح بـ.. \n2. تشغيل جهاز.. \n3. توثيق السجل.");
     }
 });
+    else if (text === '4') {
+        try {
+            const media = MessageMedia.fromFilePath('./security.pdf');
+            await client.sendMessage(msg.from, media, { caption: '📄 تفضل، هذا هو جدول دوام الأمن المعتمد.' });
+        } catch (error) {
+            console.error('خطأ في إرسال الملف:', error);
+            await msg.reply("⚠️ عذراً، حدث خطأ أثناء محاولة إرسال ملف الجدول.");
+        }
+    }
+});
 
 app.listen(8080, '0.0.0.0');
 client.initialize();
-
-
